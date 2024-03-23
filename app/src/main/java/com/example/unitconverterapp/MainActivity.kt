fcbd2ea89f887a7935c86a52226813bac27bad21
KrishKoria <krishkoria2004@gmail.com>
2024-03-23 20:04:30 +0530

    Added Spacers

--- app/src/main/java/com/example/unitconverterapp/MainActivity.kt
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.kt
@@ -3,15 +3,23 @@ package com.example.unitconverterapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,13 +44,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+
         Text(text = "Unit Converter")
-            OutlinedTextField(value = "Enter a Value", onValueChange = {})
-            Row {
-                OutlinedTextField(value = "From", onValueChange = {}, modifier = Modifier.weight(1f))
-                OutlinedTextField(value = "To", onValueChange = {}, modifier = Modifier.weight(1f))
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = "Enter a Value", onValueChange = {})
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Box(modifier = Modifier) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Dropdown Arrow")
+                }
             }
+            Box(modifier = Modifier) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Dropdown Arrow")
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Result: ")
     }
 }
